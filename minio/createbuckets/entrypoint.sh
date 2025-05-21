663ba981290a011159faa082d984fd83742c532f
makinoharafan1 <niugnep123lol@gmail.com>
2025-05-21 07:44:56 +0600

    vite+react build in s3 bucket tested

--- minio/createbuckets/entrypoint.sh
+++ minio/createbuckets/entrypoint.sh
@@ -2,11 +2,14 @@
 sleep 5 
 mc alias set dockerminio http://minio:${MINIO_PORT} ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD} 
 
-mc mb --ignore-existing dockerminio/blockscontent/temp/images/.keep
-mc mb --ignore-existing dockerminio/blockscontent/temp/files/.keep
-mc mb --ignore-existing dockerminio/blockscontent/images/.keep
-mc mb --ignore-existing dockerminio/blockscontent/files/.keep
+mc mb --ignore-existing dockerminio/blockscontent
 
 mc ilm import dockerminio/blockscontent < temp-lifecycle-config.json
 mc ilm ls dockerminio/blockscontent
+
+mc mb --ignore-existing dockerminio/react-app
+mc anonymous set download dockerminio/react-app
+
+mc mirror react-app/dist dockerminio/react-app/
+
 exit 0
